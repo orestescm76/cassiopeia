295aa13e7bb9857d752f316dcf3a0f9c0378af6f
Orestes Colomina Monsalve <orestescm@gmail.com>
2024-01-29 18:30:30 +0100

    init avalonia

--- src/Classes/ListViewPhysicalAlbum.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Forms;
-namespace Cassiopeia.src.Classes
-{
-    internal class ListViewPhysicalAlbum : ListViewItem
-    {
-        public ListViewPhysicalAlbum(string[] data) : base(data) { }
-        public string ID { get; set; }
-    }
-}
